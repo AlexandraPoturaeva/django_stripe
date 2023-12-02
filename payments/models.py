72605df6d4540b6ebe2049741b38e949b2a2cbdb
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-12-02 17:52:47 +0300

    create models Tax and Discount and connect them to Order

--- payments/models.py
+++ payments/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from core.models import TimeStampedModel
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Item(TimeStampedModel):
@@ -18,6 +19,52 @@ class Item(TimeStampedModel):
         return "{0:.2f}".format(self.price / 100)
 
 
+class Tax(TimeStampedModel):
+    display_name = models.CharField(max_length=10)
+    inclusive = models.BooleanField()
+    percentage = models.DecimalField(
+        max_digits=7,
+        decimal_places=4,
+        validators=[MinValueValidator(0)],
+    )
+    country = models.CharField(null=True, blank=True, max_length=2)
+    state = models.CharField(null=True, blank=True, max_length=50)
+    jurisdiction = models.CharField(null=True, blank=True, max_length=100)
+    description = models.CharField(null=True, blank=True, max_length=255)
+
+    def __str__(self):
+        return f'{self.display_name} {self.percentage}%'
+
+
+class Discount(TimeStampedModel):
+    amount_off = models.PositiveSmallIntegerField(null=True, blank=True)
+    currency = models.CharField(max_length=3, null=True, blank=True)
+    duration = models.CharField(
+        max_length=9,
+        choices=[
+            ('once', 'once'),
+            ('repeating', 'repeating'),
+            ('forever', 'forever'),
+        ],
+    )
+    duration_in_months = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True,
+    )
+    metadata = models.JSONField(null=True, blank=True)
+    name = models.CharField(max_length=20, null=True, blank=True)
+    percent_off = models.DecimalField(
+        max_digits=7,
+        decimal_places=4,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100),
+        ],
+        null=True,
+        blank=True,
+    )
+
+
 class Order(TimeStampedModel):
     class Meta:
         get_latest_by = 'created_at'
@@ -34,7 +81,12 @@ class Order(TimeStampedModel):
     )
     session_id = models.CharField(
         max_length=40,
-        null=False,
+    )
+    taxes = models.ManyToManyField(Tax)
+    discount = models.ForeignKey(
+        Discount,
+        on_delete=models.SET_NULL,
+        null=True,
     )
 
     def __str__(self):
